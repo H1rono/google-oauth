31ed7b0bb3ddbc2b3d38af0f547134cdbebfda97
H1rono <hronok66@gmail.com>
2024-11-29 02:54:20 +0900

    Fix `scope` API

--- src/scope.rs
+++ src/scope.rs
@@ -1,7 +1,13 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, fmt};
 
 mod private {
     pub trait Sealed {}
+}
+
+macro_rules! box_scope {
+    ($e:expr) => {
+        BoxScope(Box::new($e))
+    };
 }
 
 pub trait SingleScope: private::Sealed + Send + Sync + 'static {
@@ -17,29 +23,19 @@ pub trait Scope: private::Sealed + Send + Sync + 'static {
     }
 
     fn boxed_clone(&self) -> BoxScope;
-}
 
-macro_rules! box_scope {
-    ($e:expr) => {
-        BoxScope(Box::new($e))
-    };
-}
-
-impl<S: SingleScope> Scope for S
-where
-    S: Clone + 'static,
-{
-    fn scope(&self) -> HashSet<&'static str> {
-        [self.as_str()].into_iter().collect()
+    fn with<S: Scope>(self, scope: S) -> With<Self, S>
+    where
+        Self: Sized,
+    {
+        With(self, scope)
     }
 
-    fn grants(&self, other: &dyn SingleScope) -> bool {
-        let other = other.as_str();
-        self.as_str() == other
-    }
-
-    fn boxed_clone(&self) -> BoxScope {
-        box_scope!(self.clone())
+    fn into_boxed(self) -> BoxScope
+    where
+        Self: Sized,
+    {
+        box_scope!(self)
     }
 }
 
@@ -79,88 +75,35 @@ impl Scope for BoxScope {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-#[repr(transparent)]
-pub struct Combine<S = NoScope> {
-    inner: S,
-}
-
-impl Default for Combine<NoScope> {
-    fn default() -> Self {
-        Self::new()
+impl Clone for BoxScope {
+    fn clone(&self) -> Self {
+        self.boxed_clone()
     }
 }
 
-impl Combine<NoScope> {
-    pub fn new() -> Self {
-        Self { inner: NoScope }
+impl fmt::Debug for BoxScope {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("BoxScope").field(&self.scope()).finish()
     }
 }
 
-impl<S> Combine<S> {
-    fn wrap_ref(s: &S) -> &Self {
-        #[allow(unsafe_code)]
-        unsafe {
-            &*(s as *const S as *const Self)
-        }
-    }
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
+pub struct With<A, B>(A, B);
 
-    pub fn with<T: SingleScope>(self, single_scope: T) -> Combine<(T, S)> {
-        let Self { inner } = self;
-        Combine {
-            inner: (single_scope, inner),
-        }
-    }
+impl<A: private::Sealed, B: private::Sealed> private::Sealed for With<A, B> {}
 
-    pub fn into_boxed(self) -> BoxScope
-    where
-        Self: Scope + Clone + 'static,
-    {
-        box_scope!(self)
-    }
-}
-
-impl<S, T> Combine<(S, T)>
+impl<A, B> Scope for With<A, B>
 where
-    S: SingleScope,
-    Combine<T>: Scope,
-{
-    fn unnest_scope(s: &S, t: &T) -> HashSet<&'static str> {
-        let mut scope = Combine::wrap_ref(t).scope();
-        scope.insert(s.as_str());
-        scope
-    }
-}
-
-impl<S> private::Sealed for Combine<S> {}
-
-impl Scope for Combine<NoScope> {
-    #[inline]
-    fn scope(&self) -> HashSet<&'static str> {
-        self.inner.scope()
-    }
-
-    #[inline]
-    fn grants(&self, other: &dyn SingleScope) -> bool {
-        self.inner.grants(other)
-    }
-
-    #[inline]
-    fn boxed_clone(&self) -> BoxScope {
-        box_scope!(*self)
-    }
-}
-
-impl<S, T> Scope for Combine<(S, T)>
-where
-    S: SingleScope,
-    T: Send + Sync + 'static,
-    Combine<T>: Scope,
+    A: Scope,
+    B: Scope,
     Self: Clone,
 {
     fn scope(&self) -> HashSet<&'static str> {
-        let Self { inner: (s, t) } = self;
-        Self::unnest_scope(s, t)
+        let Self(a, b) = self;
+        let mut scope_a = a.scope();
+        let scope_b = b.scope();
+        scope_a.extend(scope_b);
+        scope_a
     }
 
     fn boxed_clone(&self) -> BoxScope {
@@ -187,9 +130,11 @@ macro_rules! scope {
             pub fn new() -> Self {
                 Self
             }
+        }
 
-            pub fn with<S: SingleScope>(self, other: S) -> Combine<(S, (Self, NoScope))> {
-                Combine::new().with(self).with(other)
+        impl fmt::Display for [< $i0:camel $( $i:camel )* >] {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.write_str(Self::STR)
             }
         }
 
@@ -198,6 +143,20 @@ macro_rules! scope {
         impl SingleScope for [< $i0:camel $( $i:camel )* >] {
             fn as_str(&self) -> &'static str {
                 Self::STR
+            }
+        }
+
+        impl Scope for [< $i0:camel $( $i:camel )* >] {
+            fn scope(&self) -> HashSet<&'static str> {
+                [Self::STR].into_iter().collect()
+            }
+
+            fn grants(&self, other: &dyn SingleScope) -> bool {
+                Self::STR == other.as_str()
+            }
+
+            fn boxed_clone(&self) -> BoxScope {
+                box_scope!(*self)
             }
         }
     )+ } };
@@ -221,9 +180,14 @@ scope! {
 /// ```
 #[macro_export]
 macro_rules! combine_scope {
-    [ $( $i0:ident $(. $i:ident)* ),* $(,)? ] => { ::paste::paste! {
-        $crate::scope::Combine::new() $(
+    [
+        $hi0:ident $(. $hi:ident)*
+        $(, $i0:ident $(. $i:ident)* )*
+        $(,)?
+    ] => { ::paste::paste! { {
+        use $crate::scope::Scope;
+        $crate::scope::[< $hi0:camel $( $hi:camel )* >] $(
             .with( $crate::scope::[< $i0:camel $( $i:camel )* >] )
         )*
-    } };
+    } } };
 }

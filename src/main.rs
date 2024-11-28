5a1db6f9ab8d43a45809382dc9113835f5d90423
H1rono <hronok66@gmail.com>
2024-11-28 13:20:12 +0900

    Impl main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use std::net::SocketAddr;
+
+use google_oauth::make_router;
+use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::EnvFilter;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into());
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
+
+    let port = std::env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()?;
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    tracing::info!("listening on {addr}");
+    let layer = tower::ServiceBuilder::new().layer(TraceLayer::new_for_http());
+    let router = make_router().layer(layer);
+    let listener = TcpListener::bind(addr).await?;
+    axum::serve(listener, router).await?;
     Ok(())
 }

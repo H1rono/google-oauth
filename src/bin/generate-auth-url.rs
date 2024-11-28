5c01beafca53fa3db07234d627f53a07e60abf1b
H1rono <hronok66@gmail.com>
2024-11-28 12:24:22 +0900

    Add test script `generate-auth-url`

---
+++ src/bin/generate-auth-url.rs
@@ -0,0 +1,14 @@
+use google_oauth::{ClientSecret, UnauthorizedClient};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let secret_file = tokio::fs::File::open("tmp/client_secret.json").await?;
+    let secret = ClientSecret::read_from_file(secret_file).await?;
+    let client = UnauthorizedClient::builder()
+        .redirect_uri("http://localhost:8080/oauth2/callback")
+        .add_scope("https://www.googleapis.com/auth/calendar")
+        .secret(&secret.web)
+        .build()?;
+    println!("{}", client.generate_url());
+    Ok(())
+}

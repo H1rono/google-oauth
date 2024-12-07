cb5172565401076bba8ee290dde1075d56fa6202
H1rono <hronok66@gmail.com>
2024-12-07 14:14:37 +0900

    Split mod `client` to `client::misc`

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,14 @@
-use std::{borrow::Cow, fmt, str::FromStr};
+use std::borrow::Cow;
 
-use serde::{de, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
 use crate::scope::{self, Scope, SpaceDelimitedScope};
 use crate::secret::WebClientSecret;
 
 pub mod calendar;
+mod misc;
+
+pub use misc::{AuthorizationCode, Bearer};
 
 #[derive(Debug, Clone)]
 pub struct ClientConfig {
@@ -210,69 +213,6 @@ impl<S1> UnauthorizedClientBuilder<S1> {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct AuthorizationCode(());
-
-struct AuthorizationCodeVisitor;
-
-impl AuthorizationCode {
-    pub const STR: &'static str = "authorization_code";
-
-    pub fn new() -> Self {
-        Self(())
-    }
-}
-
-impl fmt::Display for AuthorizationCode {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(Self::STR)
-    }
-}
-
-impl FromStr for AuthorizationCode {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s == Self::STR {
-            return Ok(Self::new());
-        }
-        Err("not authorization_code")
-    }
-}
-
-impl Serialize for AuthorizationCode {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_str(Self::STR)
-    }
-}
-
-impl<'de> Deserialize<'de> for AuthorizationCode {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: de::Deserializer<'de>,
-    {
-        deserializer.deserialize_str(AuthorizationCodeVisitor)
-    }
-}
-
-impl<'de> de::Visitor<'de> for AuthorizationCodeVisitor {
-    type Value = AuthorizationCode;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str(r#"a str "authorization_code""#)
-    }
-
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        v.parse().map_err(E::custom)
-    }
-}
-
 #[derive(PartialEq, Eq, Hash, Serialize, Deserialize)]
 struct TokenRequest<'a> {
     #[serde(borrow)]
@@ -309,70 +249,6 @@ impl<'a> TokenRequest<'a> {
         let grant_type = grant_type.to_string();
         let params = encode_queries![client_id, client_secret, code, grant_type, redirect_uri];
         params.join("&")
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct Bearer(());
-
-struct BearerVisitor;
-
-impl Bearer {
-    pub const STR: &'static str = "Bearer";
-
-    fn new() -> Self {
-        Self(())
-    }
-}
-
-impl fmt::Display for Bearer {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(Self::STR)
-    }
-}
-
-impl FromStr for Bearer {
-    type Err = &'static str;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s == Self::STR {
-            Ok(Self::new())
-        } else {
-            Err("not Bearer")
-        }
-    }
-}
-
-impl Serialize for Bearer {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        serializer.serialize_str(Self::STR)
-    }
-}
-
-impl<'de> Deserialize<'de> for Bearer {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: de::Deserializer<'de>,
-    {
-        deserializer.deserialize_str(BearerVisitor)
-    }
-}
-
-impl<'de> de::Visitor<'de> for BearerVisitor {
-    type Value = Bearer;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str(r#"a str "Bearer""#)
-    }
-
-    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        v.parse().map_err(E::custom)
     }
 }
 
